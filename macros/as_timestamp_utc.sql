670e5619d8f71c06a647676962fd2957b1388081
Leah Nguyen <nduongthucanh@gmail.com>
2023-06-11 12:15:32 +1000

    allow switching between postgres and databricks syntax

--- macros/as_timestamp_utc.sql
+++ macros/as_timestamp_utc.sql
@@ -1,3 +1,16 @@
 {% macro as_timestamp_utc(column_name) %}
+    
+    {{ return(adapter.dispatch('as_timestamp_utc')(column_name)) }}
+
+{% endmacro %}
+
+{% macro postgres__as_timestamp_utc(column_name) %}
+    -- Generate PostgreSQL syntax
     to_timestamp({{ column_name }},'YYYY-MM-DD HH24:MI:SS.MS') :: timestamp at TIME zone 'UTC'
+{% endmacro %}
+
+{% macro databricks__as_timestamp_utc(column_name) %}
+    -- Generate Databricks syntax
+    from_utc_timestamp(from_unixtime(unix_timestamp({{ column_name }}, 'yyyy-MM-dd HH:mm:ss.SSS')), 'UTC')
+
 {% endmacro %}
