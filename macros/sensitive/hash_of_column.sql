7e18ee2ea432f463a7b71113b202aa16f16be856
Leah Nguyen <leah@glamcorner.com.au>
2023-06-11 17:10:44 +1000

    Revert "implement PII on sensitive columns"

--- macros/sensitive/hash_of_column.sql
+++
@@ -1,8 +0,0 @@
-{% macro hash_of_column(column) %}
-    -- Check if the column value is null, and return null if it is
-    -- Otherwise, encode the sensitive column as '***********'
-    case 
-        when {{ column|lower }} is null then null 
-    else '***********' 
-    end as {{column|lower}}_hash,
-{% endmacro %}
